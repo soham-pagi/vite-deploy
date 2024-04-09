a62904f469f6a9b1ae136a1c573f5737bce7c706
Soham Pagi <paguisoham36@gmail.com>
2024-04-09 23:57:21 +0530

    add: environment secret

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 function App() {
+  const testVar = import.meta.env.VITE_TEST_VAR;
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <h1>env variable: {testVar}</h1>
+    </>
   )
 }
 
